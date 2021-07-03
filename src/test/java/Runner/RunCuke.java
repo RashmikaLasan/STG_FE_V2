6ba0cffe7d53479f28e89febaa791da87ecd213e
RashmikaLasan <lasanrash@gmail.com>
2021-07-03 21:04:41 +0530

    Hotel Flow Promotion Flow

--- src/test/java/Runner/RunCuke.java
+++ src/test/java/Runner/RunCuke.java
@@ -15,7 +15,7 @@ import java.util.Map;
         plugin = {"json:target/positive/cucumber.json", "pretty", "html:target/positive/cucumber.html","com.cucumber.listener.ExtentCucumberFormatter"},
         features = "src\\test\\resources\\Features",
         glue = "Steps",
-        tags = {"@Htlpromo"}
+        tags = {"@Htlcancel"}
 )
 
 public class RunCuke extends AbstractTestNGCucumberTests {
